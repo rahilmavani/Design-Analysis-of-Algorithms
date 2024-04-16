64d1a8cf51a274949f28e914b8a97acd9a63a670
Rahil Mavani <mavanirahil6@gmail.com>
2024-04-16 21:56:11 +0530

    Quick Sort

---
+++ quick-sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int lo, int hi) {
+    int pivot = arr[lo];
+    int i = lo;
+    int j = hi;
+    while (i < j) {
+        while (arr[i] <= pivot) {
+            i++;
+        }
+        while (arr[j] >= pivot) {
+            j--;
+        }
+        if (i < j) {
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[lo], &arr[j]);
+    return j;
+}
+
+
+void quick_sort(int arr[], int lo, int hi) {
+    if(lo < hi) {
+        int j = partition(arr, lo, hi);
+        quick_sort(arr, lo, j);
+        quick_sort(arr, j+1, hi);
+    }
+}
+
+int main() {
+    int arr[] = {2, 4, 1, 0, 5}; 
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quick_sort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
