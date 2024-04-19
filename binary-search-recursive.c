3f07a89f5b5ae0133299e663da9097f1d62afa2c
Rahil Mavani <mavanirahil6@gmail.com>
2024-04-20 00:20:30 +0530

    Binaru Search (Recursive)

---
+++ binary-search-recursive.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int binary_search(int array[], int size, int key, int start, int end)
+{
+    if(start > end)
+    {
+        return -1;
+    }    
+    int mid = (start + end) / 2;
+    if(array[mid] == key)
+    {
+        return mid;
+    }
+    else if(array[mid] > key)
+    {
+        return binary_search(array, size, key, start, mid - 1);
+    }
+    else
+    {
+        return binary_search(array, size, key, start, mid + 1);
+    }
+}
+
+int main()
+{
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    printf("Enter elements of the sorted array: ");
+    int array[size];
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+
+    int key;
+    printf("Enter element to be searched: ");
+    scanf("%d", &key);
+
+    int index = binary_search(array, size, key, 0, size - 1);
+
+    if (index >= 0)
+    {
+        printf("%d found at index %d", key, index);
+    }
+    if (index == -1) {
+        printf("Element not found.");
+    }
+}
