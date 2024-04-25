e74feb99812a89afe402c98b02c65a72850b8af7
Rahil Mavani <mavanirahil6@gmail.com>
2024-04-26 02:02:42 +0530

    N Queens Problem

--- n-queens.c
+++ n-queens.c
@@ -4,97 +4,9 @@
 int n;
 int count = 0;
 
-void print_board(char board[n][n])
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%c ", board[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void helper(char board[n][n], int row)
-{
-    if (n == row)
-    {
-        count++;
-        printf("Chess board number: %d\n", count);
-        print_board(board);
-        return;
-    }
-}
-
-bool is_safe(char board[n][n], int row, int col)
-{   
-    //Vertical check
-    for (int i = 0; i < n; i++)
-    {
-        if (board[row][i] == 'Q')
-            return false;
-    }
-
-    //Horizontal check
-    for (int i = row - 1; i >= 0; i--)
-    {
-        if (board[i][col] == 'Q')
-            return false;
-    }
-
-    //Lower left check
-    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
-    {
-        if (board[i][j] == 'Q')
-            return false;
-    }
-
-    //Lower right check
-    for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
-    {
-        if (board[i][j] == 'Q')
-            return false;
-    }
-
-    //Upper left check
-    for (int i = row + 1, j = col - 1; i < n && j >= 0; i++, j--)
-    {
-        if (board[i][j] == 'Q')
-            return false;
-    }
-
-    //Upper right check
-    for (int i = row + 1, j = col + 1; i < n && j < n; i++, j++)
-    {
-        if (board[i][j] == 'Q')
-            return false;
-    }
-
-    return true;
-}
-
-void n_queens(char board[n][n], int row)
-{
-
-    if (n == row)
-    {
-        count++;
-        printf("\nChess board %d\n", count);
-        print_board(board);
-        return;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        if (is_safe(board, row, i))
-        {
-            board[row][i] = 'Q';
-            n_queens(board, row + 1);
-            board[row][i] = '*';
-        }
-    }
-}
+bool isSafe(char board[n][n], int row, int col);
+void printBoard(char (*board)[n]);
+void nqueen(char board[n][n], int row);
 
 int main()
 {
@@ -111,7 +23,67 @@ int main()
         }
     }
 
-    n_queens(board, 0);
+    nqueen(board, 0);
 
     return 0;
+}
+
+void nqueen(char board[n][n], int row)
+{
+
+    if (n == row)
+    {
+        count++;
+        printf("\nChess board %d\n", count);
+        printBoard(board);
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (isSafe(board, row, i))
+        {
+            board[row][i] = 'Q';
+            nqueen(board, row + 1);
+            board[row][i] = '*';
+        }
+    }
+}
+
+bool isSafe(char board[n][n], int row, int col)
+{
+
+    // vertical check
+    for (int i = row - 1; i >= 0; i--)
+    {
+        if (board[i][col] == 'Q')
+            return false;
+    }
+
+    // upper left
+    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+    {
+        if (board[i][j] == 'Q')
+            return false;
+    }
+
+    // upper right
+    for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+    {
+        if (board[i][j] == 'Q')
+            return false;
+    }
+    return true;
+}
+
+void printBoard(char (*board)[n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
 }
